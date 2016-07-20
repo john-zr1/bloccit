535b3618ea7d7f9cf7d48d5ad0343b06d5aa940f
Jonathan Hernandez <johnizr1@gmail.com>
2016-07-19 22:36:14 -0700

    assignment 33 completed

---
+++ spec/controllers/advertisements_controller_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+require RandomData
+RSpec.describe AdvertisementsController, type: :controller do
+  let(:my_ad) do
+    Advertisement.create
+    id: 1,
+    title: RandomData.random_sentence,
+    copy: RandomData.random_paragraph,
+    price: 99,
+end
+
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it "assigns [my_ad] to @advertisements" do
+      get :index
+      expect(assigns(:advertisements)).to eq([my_ad])
+    end
+  end
+
+  describe "GET #show" do
+     it "returns http success" do
+       get :show, {id: my_ad.id}
+       expect(response).to have_http_status(:success)
+     end
+
+     it "renders the #show view" do
+       get :show, {id: my_ad_id}
+       expect(response).to render_template :show
+     end
+
+     it "assigns my_ad to @advertisement" do
+       get :show, {id: my_ad.id}
+       expect(assigns(:advertisement)).to eq(my_ad)
+    end
+  end
+
+  it "assignsthe new advertisement to @advertisements by 1" do
+    expect{post :create, advertisement: {title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 99}}.to
+    change(Advertisement, :count).by(1)
+  end
+
+  it "assigns the new advertisement to @advertisement" do
+    post :create, advertisement: {title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 99}
+    expect(assigns(:advertisement)).to eq Advertisement.last
+  end
+
+  it "redirects to the new advertisement" do
+    post :create, advertisement: {title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 99}
+    expect(response).to redirect_to Advertisement.last
+  end
+  end
+end
