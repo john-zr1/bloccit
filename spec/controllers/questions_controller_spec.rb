c1b72b90120e044d6b4d015a713ff79b93b36625
Jonathan Hernandez <johnizr1@gmail.com>
2016-07-20 19:17:41 -0700

    assignment 33 not completed

---
+++ spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+include RandomData
+
+RSpec.describe QuestionsController, type: :controller do
+
+  let (:my_question) do
+    Question.create(
+    id: 1,
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    resolved: false
+    )
+  end
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it "assigns my_question to @question" do
+      get :index
+      expect(assigns(:questions)).to eq([my_question])
+    end
+  end
+
+  describe "GET show" do
+    it "returns http success" do
+      get :show, {id: my_question.id}
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #show view" do
+      get :show, {id: my_question.id}
+      expect(response).to render_template :show
+    end
+
+    it "assigns my_question to @question" do
+      get :show, {id: my_question.id}
+      expect(assigns(:question)).to eq(my_question)
+    end
+  end
+
+  describe "GEt new" do
+    it "returnshttp success" do
+      get :newexpect(response).to have_http_status(:success)
+    end
+
+    it "renders the #new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+
+    it "instantiates @questions" do
+      get :new
+      expect(assigns(:question)).not_to be_nil
+    end
+  end
+end
