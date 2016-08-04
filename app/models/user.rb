910f794e9c77ade7e9243cc4a2bb97b78dd256b1
Jonathan Hernandez <johnizr1@gmail.com>
2016-08-03 20:54:43 -0700

    assignment 36 completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,27 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+  before_save :format_name
 
   before_save { self.email = email.downcase if email.present? }
 
-# #3
-   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
-# #4
+  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, unless: :password_digest
   validates :password, length: { minimum: 6 }, allow_blank: true
-# #5
   validates :email,
           presence: true,
           uniqueness: { case_sensitive: false },
           length: { minimum: 3, maximum: 254 }
 
-# #6
   has_secure_password
+
+  def format_name
+    if name
+      name_array = []
+      name.split.each do |name_part|
+        name_array << name_part.capitalize
+      end
+
+      self.name = name_array.join(" ")
+    end
+  end
 end
