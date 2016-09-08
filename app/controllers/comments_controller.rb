4d3847570aa8f3b542eff0e0d60a0a0735963cda
Jonathan Hernandez <johnizr1@gmail.com>
2016-09-08 00:39:26 -0700

    checkpoint 41: incompleted

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,8 @@ class CommentsController < ApplicationController
 
  before_action :require_sign_in
 
+ before_action :require_sign_in
+
  def create
 # #11
    @post = Post.find(params[:post_id])
@@ -19,10 +21,32 @@ class CommentsController < ApplicationController
    end
  end
 
+  def destroy
+    @post = Post.find(params[:post_id])
+    comment = @post.comments.find(params[:id])
+
+    if comment.destroy
+      flash[:notice] = "Comment was deleted successfully."
+      redirect_to [@post.topic, @post]
+    else
+      flash[:alert] = "Comment couldn't be deleted. Try again."
+      redirect_to [@post.topic, @post]
+    end
+  end
+
  private
 
 # #14
  def comment_params
    params.require(:comment).permit(:body)
  end
+
+# #16
+  def authorize_user
+    comment = Comment.find(params[:id])
+    unless current_user == comment.user || current_user.admin?
+      flash[:alert] = "You do not have permission to delete a comment."
+      redirect_to [comment.post.topic, comment.post]
+    end
+  end
 end
