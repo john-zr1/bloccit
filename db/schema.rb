efa78db5b17b88da9f059a310e13ec0ff82767c0
Jonathan Hernandez <johnizr1@gmail.com>
2016-09-23 21:35:20 -0700

    checkpoint 42

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811074037) do
+ActiveRecord::Schema.define(version: 20160916004511) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20160811074037) do
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+
+  create_table "labels", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -33,17 +39,6 @@ ActiveRecord::Schema.define(version: 20160811074037) do
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
-
-  create_table "sponsored_posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.integer  "price"
-    t.integer  "topic_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "sponsored_posts", ["topic_id"], name: "index_sponsored_posts_on_topic_id"
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
